51990707be7ca25c26336acdf45d2b21c4dfc031
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2022-01-25 15:03:19 +0100

    Added controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,57 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+
+    if @article.destroy
+      flash.notice = "Artikel gelöscht"
+      redirect_to articles_path
+    else
+      flash.error = "Artikel konnte nicht gelöscht werden"
+      redirect_to articles_path
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(
+      :title,
+      :text
+    )
+  end
+end
