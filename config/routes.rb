19dd60bbcbe7183d4be4857c1e12bab6c62de16d
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2022-01-27 15:32:49 +0100

    Added nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+
+  # resources :comments
 
   # get 'articles' => 'articles#index'
   # get 'articles/new' => 'articles#new'
